22ade2a830060a8b1b7cc2b01fe2cd212c08bf8e
Kirsten Keels <keelsk@gmail.com>
2020-03-22 01:46:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,8 +17,12 @@ class Application
         @@cart << add_item
       end
     elsif req.path.match(/cart/)
-      @@cart.each do |item|
-        resp.write "#{item}\n"
+      if @@cart.length == 0 
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
